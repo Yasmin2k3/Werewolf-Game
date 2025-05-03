c1b4ac21f80dbd2441ad332fd620b42b7843e10f
Mistor Ronan <bluelegendsdev@gmail.com>
2025-05-03 16:57:23 +0200

    Absolute Cinema

--- app/src/App.js
+++ app/src/App.js
@@ -10,10 +10,8 @@ function AlertBingle()
 function App() {
   return (
     <div className="App">
-      <h1>here is a button :0</h1>
-      <button onClick={AlertBingle}>The Button</button>
-      <h1>here is an example of compents and properties</h1>
-      <ExampleComponent></ExampleComponent>
+      <h2>here is an example of compents and properties</h2>
+      <ExampleComponent playerName="Anna" role="Consort"></ExampleComponent>
     </div>
   );
 }
---
+++ app/src/Components/ExampleComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ExampleComponent({playerName, role}) {
+  return (
+    <div>
+      <h1> Player name: {playerName}</h1>
+      <h1> Role: {role}</h1>
+    </div>
+  )
+}
