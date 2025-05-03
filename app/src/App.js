bec1307a73d1bfb51b121ad10205c694b7e1a651
Mistor Ronan <bluelegendsdev@gmail.com>
2025-05-03 15:46:45 +0200

    April's branch :3 doing stuff

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import ExampleComponent from "./Components/ExampleComponent";
 
+function AlertBingle()
+{
+  alert("Bingle!");
+
+}
 
 function App() {
   return (
     <div className="App">
       <h1>here is a button :0</h1>
-      <button>The Button</button>
+      <button onClick={AlertBingle}>The Button</button>
+      <h1>here is an example of compents and properties</h1>
+      <ExampleComponent></ExampleComponent>
     </div>
   );
 }
